0ead91fd09133b3bc017f4668983a18077a5eb29
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2025-03-26 11:50:22 +0100

    add reka UI along with tailwind and tailwind-variants for styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
+  css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  pages: true,
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/test-utils'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxtjs/color-mode",
+    ["reka-ui/nuxt", { prefix: "Reka" }],
+  ],
+  pages: true,
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  content: {
+    preview: {
+      api: "https://api.nuxt.studio",
+      dev: true,
+    },
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+});
