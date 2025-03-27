99d44e448ac684493ce9c685bf179ce6f0a9f0e0
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2025-03-27 09:58:00 +0100

    change to PascalCase

---
+++ components/ColorMode.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
+</script>
+
+<template>
+  <ClientOnly v-if="!colorMode?.forced">
+    <RekaSwitchRoot
+      v-model="isDark"
+      class="border data-[state=unchecked]:bg-stone-300 w-[32px] data-[state=checked]:bg-stone-500"
+    >
+      <RekaSwitchThumb
+        class="block w-3.5 my-auto transition-transform translate-x-0.5 will-change-transform data-[state=checked]:translate-x-full"
+      >
+        <Icon :name="isDark ? 'i-lucide-moon' : 'i-lucide-sun'" />
+      </RekaSwitchThumb>
+    </RekaSwitchRoot>
+  </ClientOnly>
+</template>
---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const year = new Date().getUTCFullYear();
+const copyrightPeriod = computed(() =>
+  year === 2025 ? year : `2025 - ${year}`,
+);
+</script>
+
+<template>
+  <footer class="container text-center py-5">
+    &copy; {{ copyrightPeriod }} Mateusz Kwiatkowski
+  </footer>
+</template>
+
+<style scoped></style>
