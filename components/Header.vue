8ce6cf6dca293cf20d64fc8b4a3f44ed078dc008
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2025-03-29 20:08:52 +0100

    add Header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header class="bg-black text-white">
-    <ColorMode class="block mr-0 ml-auto bg-red-500" />
-    <Button class="bg-amber-500">oko</Button>
-    <div class="py-20">
-      <div class="container font-serif">It's me, Mateusz!</div>
+  <header>
+    <div class="flex items-center justify-between">
+      <nav class="flex gap-4 uppercase">
+        <NuxtLink to="/" active-class="font-bold">Home</NuxtLink>
+        <NuxtLink to="/blog" active-class="font-bold">Blog</NuxtLink>
+        <NuxtLink to="/about" active-class="font-bold">About</NuxtLink>
+      </nav>
+      <div class="flex items-center gap-8 text-black">
+        <Button class="button button--secondary py-1 px-1.5">
+          <Icon aria-hidden="true" name="solar:magnifer-linear" size="24" />
+        </Button>
+        <ColorMode class="button button--secondary py-1 px-1.5" />
+      </div>
     </div>
   </header>
 </template>
